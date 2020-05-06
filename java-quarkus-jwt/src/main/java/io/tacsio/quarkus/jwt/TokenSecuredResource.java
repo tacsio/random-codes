b9ecb357c3e2d16e9f572e0d2722161c4f388513
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-05-06 20:57:50 -0300

    get claims

--- java-quarkus-jwt/src/main/java/io/tacsio/quarkus/jwt/TokenSecuredResource.java
+++ java-quarkus-jwt/src/main/java/io/tacsio/quarkus/jwt/TokenSecuredResource.java
@@ -1,6 +1,13 @@
 package io.tacsio.quarkus.jwt;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
@@ -13,6 +20,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 
+import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("/jwt")
@@ -22,38 +30,51 @@ public class TokenSecuredResource {
     @Inject
     private JsonWebToken jwt;
 
-    //Not secured
+    private final Random rnd = new Random();
+
+    // Not secured
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello";
     }
-    
-    //Secured and allowed to all roles
+
+    // Secured and allowed to all roles
     @GET
     @Path("permit-all")
     @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@Context SecurityContext ctx) {
-        Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
-        boolean hasJWT = jwt.getClaimNames() != null;
+    public String hello(@Context final SecurityContext ctx) {
+        final Principal caller = ctx.getUserPrincipal();
+        final String name = caller == null ? "anonymous" : caller.getName();
+        final boolean hasJWT = jwt.getClaimNames() != null;
 
         return String.format("hello %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(),
                 ctx.getAuthenticationScheme(), hasJWT);
     }
 
-    //Secured and allowed to specific roles
+    // Secured and allowed to specific roles
     @GET
     @Path("roles-allowed")
     @RolesAllowed({ "Echoer", "Subscriber" })
     @Produces(MediaType.TEXT_PLAIN)
-    public String helloRolesAllowed(@Context SecurityContext ctx) {
-        Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
-        boolean hasJWT = jwt.getClaimNames() != null;
+    public String helloRolesAllowed(@Context final SecurityContext ctx) {
+        final Principal caller = ctx.getUserPrincipal();
+        final String name = caller == null ? "anonymous" : caller.getName();
+        final boolean hasJWT = jwt.getClaimNames() != null;
 
         return String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(),
                 ctx.getAuthenticationScheme(), hasJWT);
+    }
+
+    @GET
+    @Path("claims")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("Subscriber")
+    public String claims() {
+        Map<String, Object> claims = new HashMap<>();
+        jwt.getClaimNames().forEach(name -> claims.put(name, jwt.getClaim(name)));
+
+        return claims.toString();
     }
 }
